69266960d9e7b8f9a4925d31696a5478a253e1c4
charuKranti <51417911+charuKranti@users.noreply.github.com>
2019-06-25 20:07:56 +0530

    cha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {
+  Router,
+  // import as RouterEvent to avoid confusion with the DOM Event
+  Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd
+} from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'userlist';
+  title = 'userlistAngularApp';
+  
+  currentUrl: string;
+  constructor(private router: Router, private http: HttpClient) {
+      router.events.subscribe((event: RouterEvent) => {
+          this.navigationInterceptor(event)
+      })
+  }
+
+  // this is for getting currnt url name
+  navigationInterceptor(event: RouterEvent): void {
+      if (event instanceof NavigationStart) {
+          //this.loading = true;
+          console.log('NavigationStart ');
+      }
+      if (event instanceof NavigationEnd) {
+          //this.loading = false;
+          console.log(event.constructor.name +" currentUrl -- "+event.url);
+          this.currentUrl = (event.url === "/login" || event.url === "/") ? "" : event.url;
+      }
+  }
+
+
 }
