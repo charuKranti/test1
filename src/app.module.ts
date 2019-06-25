5f1d47b41d36b12443bece24503a5cc59c9f487d
charuKranti <51417911+charuKranti@users.noreply.github.com>
2019-06-25 20:08:10 +0530

    cha

---
+++ src/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ImageViewerModule } from 'ng2-image-viewer';
+
+import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { RecordsListComponent } from './records-list/records-list.component';
+import { DetailsComponent } from './details/details.component';
+
+const appRoute: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: RecordsListComponent},
+  { path: 'details/:id', component: DetailsComponent},
+  { path: '**', redirectTo: '',pathMatch: 'full' }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RecordsListComponent,
+    DetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoute,{useHash:true}),
+    ImageViewerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
